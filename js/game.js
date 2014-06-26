e41511457936154434e0fcc0ca9b32649e53d67b
Nicholas Loomans <beaverusiv@gmail.com>
2014-06-26 20:48:52 +1200

    Added character object to Game and progress bars!

--- js/game.js
+++ js/game.js
@@ -17,6 +17,39 @@ Game = {
         row_height: 18,
         col_width: 170
     },
+    character: {
+        name: 'Player 1',
+        level: 1,
+        race: 'Human',
+        _class: 'Barbarian',
+        exp: 0,
+        gold: 0,
+        hp: 100,
+        mp: 10,
+        str: 10,
+        dex: 10,
+        _int: 10,
+        spd: 10,
+        dmg: 5,
+        def: 2,
+        fire: 0,
+        light: 0,
+        cold: 0,
+        poison: 0,
+        helm: '',
+        amulet: '',
+        ring1: '',
+        ring2: '',
+        pauldrons: '',
+        armour: '',
+        gloves: '',
+        belt: '',
+        leggings: '',
+        greaves: '',
+        weapon: 'Wooden Stick',
+        shield: '',
+        inventory: []
+    },
     // The total width of the game screen.
     width: function() {
         return 1280;
@@ -33,6 +66,18 @@ Game = {
         Crafty.background('rgb(249, 223, 125)');
          
         // Simply start the "Game" scene to get things going
-        Crafty.scene('Game');
+        Crafty.scene('Travelling');
+    },
+
+    startProgressBar: function(_event, _percent, _callback) {
+        setTimeout(function(){
+            if(100 >= _percent) {
+                Crafty.trigger(_event, _percent);
+                _percent += 10;
+                Game.startProgressBar(_event, _percent, _callback);
+            } else {
+                _callback();
+            }
+        }, 50);
     }
 }
--- js/scenes.js
+++ js/scenes.js
@@ -1,167 +1,29 @@
 Crafty.scene('Game', function() {
-    // Minimap
-    for (var x = 0; x < Game.map_grid.width; x++) {
-        for (var y = 0; y < Game.map_grid.height; y++) {
-            var at_edge = x == 0 || x == Game.map_grid.width - 1 || y == 0 || y == Game.map_grid.height - 1;
-            if (at_edge) {
-                // Place a tree entity at the current tile
-                Crafty.e('Tree').at(x, y);
-            } else if (Math.random() < 0.06) {
-                // Place a bush entity at the current tile
-                Crafty.e('Bush').at(x, y);
-            }
-        }
-    }
+    setupMiniMap();
+    setupCharacterScreen();
+});
 
-    // LCS
-    Crafty.e('DisplayText')
-        .at(0, 0)
-        .label('Name')
-        .update('BeaverusIV');
+Crafty.scene('Travelling', function() {
+    // setupMiniMap();
+    // setupCharacterScreen();
 
-    Crafty.e('DisplayText')
-        .at(1, 0)
-        .label('Level')
-        .update(1);
+    Crafty.e("2D, DOM, ProgressBar")
+        .attr({ x: 150, y : 140, w: 100, h: 25, z: 100 })
+        // progressBar(Number maxValue, Boolean flipDirection, String emptyColor, String filledColor)
+        .progressBar(100, false, "blue", "green")
+        .bind("LOADING_PROGRESS", function(percent) {
+            // updateBarProgress(Number currentValue)
+            this.updateBarProgress(percent);
+        });
+    Game.startProgressBar("LOADING_PROGRESS", 0, function() { console.log("1 Done!"); });
 
-    Crafty.e('DisplayText')
-        .at(0, 1)
-        .label('Race')
-        .update('Human');
-
-    Crafty.e('DisplayText')
-        .at(1, 1)
-        .label('Class')
-        .update('Barbarian');
-
-    Crafty.e('DisplayText')
-        .at(0, 2)
-        .label('Exp')
-        .update(0);
-
-    Crafty.e('DisplayText')
-        .at(1, 2)
-        .label('Gold')
-        .update(0);
-
-    Crafty.e('DisplayText')
-        .at(0, 3)
-        .label('HP')
-        .update(100);
-
-    Crafty.e('DisplayText')
-        .at(1, 3)
-        .label('MP')
-        .update(10);
-
-    Crafty.e('DisplayText')
-        .at(0, 4)
-        .label('STR')
-        .update(10);
-
-    Crafty.e('DisplayText')
-        .at(1, 4)
-        .label('DEX')
-        .update(10);
-
-    Crafty.e('DisplayText')
-        .at(0, 5)
-        .label('INT')
-        .update(10);
-
-    Crafty.e('DisplayText')
-        .at(1, 5)
-        .label('SPD')
-        .update(10);
-
-    Crafty.e('DisplayText')
-        .at(0, 6)
-        .label('DEF')
-        .update(10);
-
-    Crafty.e('DisplayText')
-        .at(1, 6)
-        .label('DMG')
-        .update(10);
-
-    Crafty.e('DisplayText')
-        .at(0, 7)
-        .label('Fire')
-        .update(0);
-
-    Crafty.e('DisplayText')
-        .at(1, 7)
-        .label('Light')
-        .update(0);
-
-    Crafty.e('DisplayText')
-        .at(0, 8)
-        .label('Cold')
-        .update(0);
-
-    Crafty.e('DisplayText')
-        .at(1, 8)
-        .label('Poison')
-        .update(0);
-
-    Crafty.e('DisplayText')
-        .at(0, 9)
-        .label('Amulet')
-        .update('...');
-
-    Crafty.e('DisplayText')
-        .at(1, 9)
-        .label('Helm')
-        .update('...');
-
-    Crafty.e('DisplayText')
-        .at(0, 11)
-        .label('Ring')
-        .update('...');
-
-    Crafty.e('DisplayText')
-        .at(1, 11)
-        .label('Ring')
-        .update('...');
-
-    Crafty.e('DisplayText')
-        .at(0, 13)
-        .label('Pauldrons')
-        .update('Superior Healthy Shoulder Pads of Running');
-
-    Crafty.e('DisplayText')
-        .at(1, 13)
-        .label('Armour')
-        .update('...');
-
-    Crafty.e('DisplayText')
-        .at(0, 15)
-        .label('Gloves')
-        .update('Master Lined Mittens of Riding');
-
-    Crafty.e('DisplayText')
-        .at(1, 15)
-        .label('Belt')
-        .update('...');
-
-    Crafty.e('DisplayText')
-        .at(0, 17)
-        .label('Leggings')
-        .update('...');
-
-    Crafty.e('DisplayText')
-        .at(1, 17)
-        .label('Greaves')
-        .update('...');
-
-    Crafty.e('DisplayText')
-        .at(0, 19)
-        .label('Weapon')
-        .update('...');
-
-    Crafty.e('DisplayText')
-        .at(1, 19)
-        .label('Shield')
-        .update('...');
-
+    Crafty.e("2D, DOM, ProgressBar")
+        .attr({ x: 150, y : 280, w: 100, h: 25, z: 100 })
+        // progressBar(Number maxValue, Boolean flipDirection, String emptyColor, String filledColor)
+        .progressBar(100, false, "blue", "green")
+        .bind("LOADING", function(percent) {
+            // updateBarProgress(Number currentValue)
+            this.updateBarProgress(percent);
+        });
+    Game.startProgressBar("LOADING", 0, function() { console.log("2 Done!"); });
 });
