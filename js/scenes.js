1c4057b6b09643bc4845ede69bb8a8ad14418e3d
Nicholas Loomans <beaverusiv@gmail.com>
2014-06-26 20:55:36 +1200

    Removed old code and styles from pre-canvas

--- js/scenes.js
+++ js/scenes.js
@@ -4,26 +4,14 @@ Crafty.scene('Game', function() {
 });
 
 Crafty.scene('Travelling', function() {
-    // setupMiniMap();
-    // setupCharacterScreen();
+    setupMiniMap();
+    setupCharacterScreen();
 
     Crafty.e("2D, DOM, ProgressBar")
-        .attr({ x: 150, y : 140, w: 100, h: 25, z: 100 })
-        // progressBar(Number maxValue, Boolean flipDirection, String emptyColor, String filledColor)
+        .attr({ x: 394, y : 620, w: 100, h: 5, z: 100 })
         .progressBar(100, false, "blue", "green")
-        .bind("LOADING_PROGRESS", function(percent) {
-            // updateBarProgress(Number currentValue)
+        .bind("TRAVELLING_TO_FIGHT", function(percent) {
             this.updateBarProgress(percent);
         });
-    Game.startProgressBar("LOADING_PROGRESS", 0, function() { console.log("1 Done!"); });
-
-    Crafty.e("2D, DOM, ProgressBar")
-        .attr({ x: 150, y : 280, w: 100, h: 25, z: 100 })
-        // progressBar(Number maxValue, Boolean flipDirection, String emptyColor, String filledColor)
-        .progressBar(100, false, "blue", "green")
-        .bind("LOADING", function(percent) {
-            // updateBarProgress(Number currentValue)
-            this.updateBarProgress(percent);
-        });
-    Game.startProgressBar("LOADING", 0, function() { console.log("2 Done!"); });
+    Game.startProgressBar("TRAVELLING_TO_FIGHT", 0, function() { console.log("1 Done!"); });
 });
