b61e48bafbf925bf62b4d3c14e5714fee8531f9b
Nicholas Loomans <beaverusiv@gmail.com>
2014-06-26 21:01:35 +1200

    Added progress text and adjusted bar position

--- js/scenes.js
+++ js/scenes.js
@@ -7,8 +7,12 @@ Crafty.scene('Travelling', function() {
     setupMiniMap();
     setupCharacterScreen();
 
+     Crafty.e('2D, DOM, Text')
+        .attr({ x: 394, y: 620, w: 300 })
+        .text('Travelling to the killing fields...');
+
     Crafty.e("2D, DOM, ProgressBar")
-        .attr({ x: 394, y : 620, w: 100, h: 5, z: 100 })
+        .attr({ x: 554, y : 622, w: 100, h: 10, z: 100 })
         .progressBar(100, false, "blue", "green")
         .bind("TRAVELLING_TO_FIGHT", function(percent) {
             this.updateBarProgress(percent);
